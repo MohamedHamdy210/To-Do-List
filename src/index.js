4080a7e4c0cb1607006de6619a629a04ba8d7907
MohamedHamdy210 <mohamed.hamdyy210@gmail.com>
2024-09-18 03:21:30 +0300

    some logic and ui

--- src/index.js
+++ src/index.js
@@ -1,8 +1,11 @@
 import "./styles.css";
+import pen from './assets/pencil.svg';
+import check from './assets/check-bold.svg'
 const peronalList=[];
 const homeList=[];
 const workList=[];
 const doneList=[];
+let taskList=JSON.parse(localStorage.getItem("home"));
 const inTitle=document.getElementById("title");
 const inDesc=document.getElementById("desc");
 const inDueDate=document.getElementById("date");
@@ -11,7 +14,9 @@ const inList=document.getElementById("list");
 const submitBtn=document.getElementById("submit");
 const form=document.getElementById("taskForm");
 const add =document.getElementById("plus");
-function addTask () {
+const content =document.getElementById("content");
+
+const addTask= () =>{
     event.preventDefault;
     const taskObj={
         id: inTitle.value.toLowerCase().split(" ").join("-")+inDueDate.value+list.value,
@@ -20,49 +25,45 @@ function addTask () {
         dueDate:inDueDate.value,
         priority:inP.value,
         list:parseInt(inList.value),
-}
+    }    
     console.log(taskObj);
     switch (taskObj.list) {
         case 1 :
-            doneList.push(taskObj);    
-            localStorage.setItem("done",JSON.stringify(todayList));
-            break;
-        case 2 :
-            peronalList.push(taskObj);    
-            localStorage.setItem("personal",JSON.stringify(peronalList));
-            break;
-        case 3 :
             homeList.push(taskObj);    
             localStorage.setItem("home",JSON.stringify(homeList));
             break;
-            
-        case 4 :
+        case 2 :
             workList.push(taskObj);    
             localStorage.setItem("work",JSON.stringify(workList));
             break;
+        case 3 :
+            peronalList.push(taskObj);    
+            localStorage.setItem("personal",JSON.stringify(peronalList));
+            break;
             
-        
+       
         default:
             alert("Error");
             return;
         
     }
-    form.classList.toggle("hidden");
+    form.classList.toggle("flex");
+    getTask(taskObj.list);
+
 }
-function getTask(list) {
-    let taskList;
+const getTask=(list)=> {
     switch (list) {
         case 1 :
-            taskList=JSON.parse(localStorage.getItem("done"));
-            break;
-        case 2 :
-            taskList=JSON.parse(localStorage.getItem("peronal"));
-            break;
-        case 3 :
             taskList=JSON.parse(localStorage.getItem("home"));
             break;
-        case 4 :
+        case 2 :
             taskList=JSON.parse(localStorage.getItem("work"));
+            break;
+        case 3 :
+            taskList=JSON.parse(localStorage.getItem("personal"));
+            break;
+        case 4 :
+            taskList=JSON.parse(localStorage.getItem("done"));
             break;    
         
         default:
@@ -72,10 +73,63 @@ function getTask(list) {
     }
     
     console.log(taskList);
-
+    
+   updateContainer(); 
+}
+const updateContainer=()=> {
+    content.innerHTML=``;
+    taskList.forEach(task => {
+        content.innerHTML+=`<div class="card ${task.priority}" id="${task.id}" >
+            <div class="info">
+                <h2>Title: ${task.title}</h1>
+                <h2>Description: ${task.description}</h1>
+                <h2>Due Date: ${task.dueDate}</h1>
+            </div>
+            
+                <button onclick="doTask(this)" type="button"><img src=${check} "></button>
+            
+        </div>`
+    });
     
 }
-add.addEventListener("click",()=>{console.log("done")})
- getTask(4);
 
+
+const doTask = (buttonEl)=>{
+    const type =buttonEl.parentElement.list ;
+    let index;
+    switch (type) {
+        case 1 :
+            
+            index=homeList.findIndex((item)=>item.id===buttonEl.parentElement.id);
+            buttonEl.parentElement.remove();
+            homeList.splice(index, 1);
+            localStorage.setItem("home", JSON.stringify(homeList));
+            break;
+        case 2 :
+            index=workList.findIndex((item)=>item.id===buttonEl.parentElement.id);
+            buttonEl.parentElement.remove();
+            workList.splice(index, 1);
+            localStorage.setItem("work", JSON.stringify(workList));
+            
+            break;
+        case 3 :
+            index=peronalList.findIndex((item)=>item.id===buttonEl.parentElement.id);
+            buttonEl.parentElement.remove();
+            peronalList.splice(index, 1);
+            localStorage.setItem("personal", JSON.stringify(peronalList));
+            
+            break;
+        
+        default:
+            alert("Error");
+            return;
+    }
+    updateContainer()
+    
+
+};
+
+updateContainer()
 submitBtn.addEventListener("click",addTask);
+add.addEventListener("click",()=>{form.classList.toggle("flex");
+});
