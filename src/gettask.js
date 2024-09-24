74fb61a3c2c643464d3e9a8eda53f115e82278d6
MohamedHamdy210 <mohamed.hamdyy210@gmail.com>
2024-09-24 19:49:49 +0300

    formatting

--- src/gettask.js
+++ src/gettask.js
@@ -1,10 +1,10 @@
 import { updateContainer } from ".";
-
-let taskList=[]
-const getTask=(list="Home")=> {
-    taskList=JSON.parse(localStorage.getItem("tasks"))||[];
-    if(taskList.length){taskList= taskList.filter((item)=>item.list==list);
-     }
-    updateContainer();
-}
-export {getTask,taskList};
+let taskList = [];
+const getTask = (list = "Home") => {
+  taskList = JSON.parse(localStorage.getItem("tasks")) || [];
+  if (taskList.length) {
+    taskList = taskList.filter((item) => item.list == list);
+  }
+  updateContainer();
+};
+export { getTask, taskList };
--- src/index.js
+++ src/index.js
@@ -1,43 +1,51 @@
 import "./styles.css";
-import check from './assets/check-bold.svg'
-import bin from './assets/delete.svg'
-import { addTask,tasks } from "./addtask";
-import { getTask , taskList} from "./gettask";
-const add =document.getElementById("plus");
-const content =document.getElementById("content");
-const form=document.getElementById("taskForm");
-const homeBtn=document.getElementById("home");
-const workBtn=document.getElementById("work");
-const personalBtn=document.getElementById("personal");
-const doneBtn=document.getElementById("done");
-const formContainer=document.getElementById("formContainer");
-window.doneTask =function (buttonEl){
-    const doneIndex=tasks.findIndex((item)=>item.id===buttonEl.parentElement.parentElement.id);
-    if(tasks[doneIndex].list!=="Done"){
-    tasks[doneIndex].list="Done";
-    localStorage.setItem("tasks",JSON.stringify(tasks));
-    const displayIndex=taskList.findIndex((item)=>item.id===buttonEl.parentElement.parentElement.id);
+import check from "./assets/check-bold.svg";
+import bin from "./assets/delete.svg";
+import { addTask, tasks } from "./addtask";
+import { getTask, taskList } from "./gettask";
+const add = document.getElementById("plus");
+const content = document.getElementById("content");
+const form = document.getElementById("taskForm");
+const homeBtn = document.getElementById("home");
+const workBtn = document.getElementById("work");
+const personalBtn = document.getElementById("personal");
+const doneBtn = document.getElementById("done");
+const formContainer = document.getElementById("formContainer");
+window.doneTask = function (buttonEl) {
+  const doneIndex = tasks.findIndex(
+    (item) => item.id === buttonEl.parentElement.parentElement.id,
+  );
+  if (tasks[doneIndex].list !== "Done") {
+    tasks[doneIndex].list = "Done";
+    localStorage.setItem("tasks", JSON.stringify(tasks));
+    const displayIndex = taskList.findIndex(
+      (item) => item.id === buttonEl.parentElement.parentElement.id,
+    );
     taskList.splice(displayIndex, 1);
 
     updateContainer();
-    }   
-}
-window.deleteTask= function (buttonEl){
-    const delIndex=tasks.findIndex((item)=>item.id===buttonEl.parentElement.parentElement.id);
-    const listIndex=taskList.findIndex((item)=>item.id===buttonEl.parentElement.parentElement.id);
-    buttonEl.parentElement.remove();
-    tasks.splice(delIndex, 1);
-    taskList.splice(listIndex, 1);
-    localStorage.setItem("tasks",JSON.stringify(tasks));
-    updateContainer()
-}
+  }
+};
+window.deleteTask = function (buttonEl) {
+  const delIndex = tasks.findIndex(
+    (item) => item.id === buttonEl.parentElement.parentElement.id,
+  );
+  const listIndex = taskList.findIndex(
+    (item) => item.id === buttonEl.parentElement.parentElement.id,
+  );
+  buttonEl.parentElement.remove();
+  tasks.splice(delIndex, 1);
+  taskList.splice(listIndex, 1);
+  localStorage.setItem("tasks", JSON.stringify(tasks));
+  updateContainer();
+};
 
-const updateContainer=()=> {
-    content.innerHTML=``;
-    if(taskList.length){
-        content.innerHTML=`<h1>${taskList[0].list}</h1>`
-        taskList.forEach(task => {
-        content.innerHTML+=`<div class="card ${task.priority} ${task.list==="Done"?"Done":""}" id="${task.id}" >
+const updateContainer = () => {
+  content.innerHTML = ``;
+  if (taskList.length) {
+    content.innerHTML = `<h1>${taskList[0].list}</h1>`;
+    taskList.forEach((task) => {
+      content.innerHTML += `<div class="card ${task.priority} ${task.list === "Done" ? "Done" : ""}" id="${task.id}" >
             <div class="info">
                 <h2>Title: ${task.title}</h1>
                 <h2>Description: ${task.description}</h1>
@@ -48,20 +56,28 @@ const updateContainer=()=> {
                <img src=${check} onclick="doneTask(this)">
            </div>
               
-        </div>`
+        </div>`;
     });
-    }
-    else{
-        content.innerHTML=`<h1>No Tasks</h1>`;
-    } 
-}
+  } else {
+    content.innerHTML = `<h1>No Tasks</h1>`;
+  }
+};
 
 getTask();
-add.addEventListener("click",()=>{form.classList.toggle("flex");
-formContainer.classList.toggle("hidden");
+add.addEventListener("click", () => {
+  form.classList.toggle("flex");
+  formContainer.classList.toggle("hidden");
 });
-homeBtn.addEventListener("click",()=>{getTask("Home")});
-workBtn.addEventListener("click",()=>{getTask("Work")});
-personalBtn.addEventListener("click",()=>{getTask("Personal")});
-doneBtn.addEventListener("click",()=>{getTask("Done")});
-export {updateContainer,};
+homeBtn.addEventListener("click", () => {
+  getTask("Home");
+});
+workBtn.addEventListener("click", () => {
+  getTask("Work");
+});
+personalBtn.addEventListener("click", () => {
+  getTask("Personal");
+});
+doneBtn.addEventListener("click", () => {
+  getTask("Done");
+});
+export { updateContainer };
